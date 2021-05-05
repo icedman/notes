3698b07fb5979bed5903eb9dcc88a07f4237cf46
icedman <m4rvin2005@gmail.com>
2021-05-05 22:36:33 +0800

    remove shell and theme (moved to gnome-shell-hammer)

--- shell/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set +x
-
-glib-compile-resources js-resources.gresource.xml
--- theme/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set +x
-
-cd theme
-glib-compile-resources gnome-shell-theme.gresource.xml
-sudo cp ./gnome-shell-theme.gresource /usr/share/gnome-shell/
-cd ..
