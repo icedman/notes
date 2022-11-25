7b664dc373d902efcdce2268ac685aa4676f50b4
icedman <m4rvin2005@gmail.com>
2022-11-25 09:32:46 +0800

    add service location at comment

--- remap.sh
+++ remap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# use with /etc/systemd/system/remap-keys.service
 evsieve --input /dev/input/event4 grab    \
         --map yield key:rightctrl key:up  \
         --map yield key:rightalt key:left   \
