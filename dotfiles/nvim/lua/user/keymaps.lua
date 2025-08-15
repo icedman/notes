cd24a3af729378fc4c390fc8f1537154f3f95089
icedman <m4rvin2005@gmail.com>
2025-08-15 17:28:17 +0800

    add clear noh

--- dotfiles/nvim/lua/user/keymaps.lua
+++ dotfiles/nvim/lua/user/keymaps.lua
@@ -72,3 +72,5 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
+-- remove highlights
+keymap("n", "<Leader><Esc>", ":noh<CR>", opts)
